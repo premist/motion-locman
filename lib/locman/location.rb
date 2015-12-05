a8e1a5b15cec32b9d49a44755808043e0bb8fc1b
Minku Lee <premist@me.com>
2015-12-05 18:26:26 +0900

    Document Locman::Location

--- lib/locman/location.rb
+++ lib/locman/location.rb
@@ -1,9 +1,30 @@
 module Locman
+  # Yolo
   class Location
-    attr_accessor :latitude, :longitude, :altitude, :floor,
-                  :accuracy, :altitude_accuracy,
-                  :determined_at
+    # @return [Float] Latitude coordinate of the location.
+    attr_accessor :latitude
 
+    # @return [Float] Longitude coordinate of the location
+    attr_accessor :longitude
+
+    # @return [Float] Altitude distance measurement of the location, in meters.
+    attr_accessor :altitude
+
+    # @return [Integer] The logical floor of the building.
+    attr_accessor :floor
+
+    # @return [Float] Latitude and longitude measurement accuracy, in meters.
+    attr_accessor :accuracy
+
+    # @return [Float] Altitude measurement accuracy, in meters.
+    attr_accessor :altitude_accuracy
+
+    # @return [Time] The time at which this location was determined
+    attr_accessor :determined_at
+
+    # Creates a new Locman::Location instance from CLLocation object.
+    # @param cl_location [CLLocation]
+    # @return [Locman::Location]
     def self.create_from_cl_location(cl_location)
       Locman::Location.new(
         latitude: cl_location.coordinate.latitude,
@@ -16,8 +37,12 @@ module Locman
       )
     end
 
-    def initialize(params = {})
-      params.each { |key, value| send("#{key}=", value) }
+    # Creates a new Locman::Location instance.
+    # @param options [Hash] Attributes that will beassigned on instance creation
+    # @return [Locman::Location]
+    def initialize(options = {})
+      options.each { |key, value| send("#{key}=", value) }
+      self
     end
   end
 end
